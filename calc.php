ede14c5f1401e53734262b4ba2d8ff92d6f37ac2
knifewifealive <62758458+knifewifealive@users.noreply.github.com>
2021-12-15 21:06:31 +0500

    Add files via upload

--- calc.php
+++ calc.php
@@ -3,8 +3,8 @@
 if ($_POST['sum'] < 1000 || $_POST['sum'] > 3000000) {
   echo 'Сумма вклада должна быть в диапазоне [1000; 3000000]';
   exit();
-} elseif ($_POST['term'] < 1 || $_POST['term'] > 60) {
-  echo 'Продолжительность вклада может быть в пределах от 1 до 60 месяцев, либо от 1 до 5 лет';
+} elseif ($_POST['term'] < 1) {
+  echo 'Продолжительность вклада должна быть больше одного месяца';
   exit();
 } elseif ($_POST['sumAdd'] < 0 || $_POST['sumAdd'] > 3000000) {
   echo 'Сумма пополнения должна быть в диапазоне [0; 3000000]';
@@ -12,7 +12,7 @@ if ($_POST['sum'] < 1000 || $_POST['sum'] > 3000000) {
 } elseif (gettype($_POST['percent']*1) !== integer || $_POST['percent'] < 3 || $_POST['percent'] > 100) {
   echo 'Процент это целое число от 3 до 100';
   exit();
-} elseif ($_POST['selectDuration'] == 'Год' && ($_POST['term'] < 1 || $_POST['term'] > 5)) {
+} elseif ($_POST['selectDuration'] == 'Год' && ($_POST['term'] < 1)) {
   echo 'Продолжительность вклада может быть в пределах от 1 до 60 месяцев, либо от 1 до 5 лет';
   exit();
 }
@@ -24,81 +24,41 @@ else $term = $_POST['term'];
 
 
 $daysN = countDaysInMonths($_POST['startDate'], $term);
-
+$daysY = countDaysInYear($_POST['startDate'], $term);
 //$sum = countDeposit($_POST['sum'], $term, $daysN);
 
-$sumAdd = countDepositAdd($_POST['sum'],$_POST['sumAdd'], $term, $daysN);
+$sumAdd = countDepositAdd($_POST['sum'],$_POST['sumAdd'], $term, $daysN, $daysY);
 echo $sumAdd;
 //Функция возвращает массив из количества дней по каждому месяцу; 1 - полученная дата, 2 - полученная продолжительность в месяцах.
-function countDaysInMonths($dateStart,$term): array
-{
-    $daysList = array();
-
-    $dateDepositOpens = date_parse_from_format("d.m.Y", $dateStart);
-    for ($i = 0, $j =0, $k = 0, $l = 0, $m = 0, $n = 0;  $i < $term; $i++) {
-        $currentMonth = $dateDepositOpens['month'];//12
-        $currentYear = $dateDepositOpens['year'];//2021
-        $allMonths = $currentMonth +$i;
-        if ($allMonths >= 13) {
-            $currentYear++;
-            $allMonths = 0;
-            ++$j;
-            $allMonths += $j;
-            if ($allMonths >= 13) {
-                $currentYear++;
-                $allMonths = 0;
-                ++$k;
-                $allMonths += $k;
-                if ($allMonths >= 13) {
-                    $currentYear++;
-                    $allMonths = 0;
-                    ++$l;
-                    $allMonths += $l;
-                    if ($allMonths >= 13) {
-                        $currentYear++;
-                        $allMonths = 0;
-                        ++$m;
-                        $allMonths += $m;
-                        if ($allMonths >= 13) {
-                            $currentYear++;
-                            $allMonths = 0;
-                            ++$n;
-                            $allMonths += $n;
-                            $number = cal_days_in_month(CAL_GREGORIAN, $allMonths, $currentYear);
-                            $daysList[] = $number;
-
-                        }
-                        $number = cal_days_in_month(CAL_GREGORIAN, $allMonths, $currentYear);
-                        $daysList[] = $number;
-                        continue;
-                    }
-                    $number = cal_days_in_month(CAL_GREGORIAN, $allMonths, $currentYear);
-                    $daysList[] = $number;
-                    continue;
-                }
-                $number = cal_days_in_month(CAL_GREGORIAN, $allMonths, $currentYear);
-                $daysList[] = $number;
-                continue;
-            }
-            $number = cal_days_in_month(CAL_GREGORIAN, $allMonths, $currentYear);
-            $daysList[] = $number;
-            continue;
-
-        }
-        $number = cal_days_in_month(CAL_GREGORIAN, $allMonths, $currentYear);
-        $daysList[] = $number;
-
-    }
-    return array_slice($daysList, 0, $term);
+function countDaysInMonths($startDate, $term) {
+  $d = new DateTime($startDate);
+  $daysN = array();
+  for ($i=0; $i < $term; $i++) { 
+    $daysN[] = cal_days_in_month(CAL_GREGORIAN , date_format($d, 'm'), date_format($d, 'Y'));
+    date_add($d, date_interval_create_from_date_string('1 month'));    
+  }
+  return $daysN;
 }
-
+//Функция считает количество дней в году для каждого месяца, исходя из даты открытия и продолжительности вклада
+function countDaysInYear($startDate, $term) {
+  $date = new DateTime($startDate);
+  $daysY = array();
+  for ($i=0; $i < $term; $i++) {
+    if(date_format($date, 'Y') % 4 == 0 && date_format($date, 'Y') % 100 != 0 || date_format($date, 'Y') % 400 == 0) {
+      $daysY[] = 366;
+    } else
+      $daysY[] = 365;
+    date_add($date, date_interval_create_from_date_string('1 month'));   
+  }
+  return $daysY;
+}
 //Функция считает вклад с пополнением
 //sumN = sum + (sum + sumAdd) * daysN * (percent / daysY)
-function countDepositAdd($sum,$sumAdd, $term, $daysN): string
+function countDepositAdd($sum,$sumAdd, $term, $daysN, $daysY): string
 {
     $percentIncome = array();
     for ($i=0; $i<$term; $i++){
-        $month_summ = (($sum + $sumAdd )* $daysN[$i] * ($_POST['percent']/100/365));
+        $month_summ = (($sum + $sumAdd )* $daysN[$i] * ($_POST['percent']/100/$daysY[$i]));
         $sum= $sum + $month_summ + $sumAdd;
         $percentIncome[] = $month_summ;
     }
